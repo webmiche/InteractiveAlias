17fe461003ef920c6a5691294545e34d104d10e5
Michel Weber <michel.weber@inf.ethz.ch>
2023-03-09 16:54:44 +0100

    refactor, add some functions

--- modify_alias/modify_alias.py
+++ modify_alias/modify_alias.py
@@ -1,6 +1,47 @@
 from subprocess import Popen, PIPE, run, DEVNULL
 
 
+def get_result_from_output(output: str) -> str:
+    return output.split(" ")[2][:-1]
+
+
+def give_same_result(p, res: str):
+    if res == "NoAlias":
+        p.stdin.write("0\n")
+    elif res == "MustAlias":
+        p.stdin.write("1\n")
+    elif res == "PartialAlias":
+        p.stdin.write("2\n")
+    elif res == "MayAlias":
+        p.stdin.write("3\n")
+
+
+# Runs the given compile command mapping AliasResults through substitutions
+#
+# Returns true if the modification was successful
+def compile_with_substitutions(cmd: list[str], substitutions: dict[str, int],
+                               output_file_name: str) -> bool:
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE, text=True)
+    line = p.stdout.readline().strip()
+    while not line.startswith("; ModuleID"):
+
+        if line.startswith("Failed"):
+            return False
+
+        curr_result = line.split(" ")[2][:-1]
+        p.stdin.write(str(substitutions[curr_result]) + "\n")
+
+        p.stdin.flush()
+        line = p.stdout.readline().strip()
+
+    output_file = open(output_file_name, 'w')
+    while line != "":
+        line = p.stdout.readline()
+        output_file.write(line)
+
+    return True
+
+
 def get_ll_file(compiler_path: str, input_file: str) -> None:
     cmd = compiler_path + " -S -emit-llvm -I/usr/include/csmith-2.3.0 -Xclang -disable-llvm-passes -o csmith/file2.ll " + input_file + " -O1"
     run(cmd.split(" "), stdout=DEVNULL, stderr=DEVNULL, text=True)
@@ -20,20 +61,10 @@ def get_count_may_alias_queries(cmd: list[str]) -> int:
         if line.startswith("Failed"):
             return -1
 
-        curr_result = line.split(" ")[2][:-1]
-        if curr_result == "NoAlias":
-            p.stdin.write("0\n")
-        elif curr_result == "MustAlias":
-            p.stdin.write("1\n")
-        elif curr_result == "PartialAlias":
-            p.stdin.write("2\n")
-        elif curr_result == "MayAlias":
-            p.stdin.write("3\n")
+        curr_result = get_result_from_output(line)
+        if curr_result == "MayAlias":
             count = count + 1
-        else:
-            print(line)
-            print("Unknown result: " + curr_result)
-            return -1
+        give_same_result(p, curr_result)
 
         p.stdin.flush()
         line = p.stdout.readline().strip()
@@ -55,21 +86,14 @@ def execute_with_modifications(cmd: list[str], modify_index: int,
         if line.startswith("Failed"):
             return False
 
-        curr_result = line.split(" ")[2][:-1]
+        curr_result = get_result_from_output(line)
         if curr_result == "MayAlias" and index == modify_index:
-            index = index + 1
             p.stdin.write(str(change_to) + "\n")
-        elif curr_result == "NoAlias":
-            p.stdin.write("0\n")
-        elif curr_result == "MustAlias":
-            p.stdin.write("1\n")
-        elif curr_result == "PartialAlias":
-            p.stdin.write("2\n")
-        elif curr_result == "MayAlias":
-            p.stdin.write("3\n")
         else:
-            print("Unknown result: " + curr_result)
-            return False
+            give_same_result(p, curr_result)
+
+        if curr_result == "MayAlias":
+            index = index + 1
 
         p.stdin.flush()
         line = p.stdout.readline().strip()
