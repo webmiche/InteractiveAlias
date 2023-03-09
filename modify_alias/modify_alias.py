a7f0cd5069157dcc813cfbf34f7071662659b203
Michel Weber <michel.weber@inf.ethz.ch>
2023-03-09 15:50:59 +0100

    parametrize, allow args

--- modify_alias/modify_alias.py
+++ modify_alias/modify_alias.py
@@ -1,12 +1,15 @@
 from subprocess import Popen, PIPE, run, DEVNULL
 
 
-def get_ll_file():
-    cmd = "../llvm-project/build_ast/bin/clang -S -emit-llvm -I/usr/include/csmith-2.3.0 -Xclang -disable-llvm-passes -o csmith/file2.ll csmith/file2.c -O1"
+def get_ll_file(compiler_path: str, input_file: str) -> None:
+    cmd = compiler_path + " -S -emit-llvm -I/usr/include/csmith-2.3.0 -Xclang -disable-llvm-passes -o csmith/file2.ll " + input_file + " -O1"
     run(cmd.split(" "), stdout=DEVNULL, stderr=DEVNULL, text=True)
 
 
-def get_count_may_alias_queries(cmd) -> int:
+# Given a command that runs opt, returns the number of MayAlias queries
+#
+# Returns -1 if the command or an input fails
+def get_count_may_alias_queries(cmd: list[str]) -> int:
 
     p = Popen(cmd, stdin=PIPE, stdout=PIPE, text=True)
     line = p.stdout.readline().strip()
@@ -41,7 +44,8 @@ def get_count_may_alias_queries(cmd) -> int:
 # Returns true if the modification was successful
 #
 # change_to is 0 for NoAlias, 1 for MusAlias, 2 for PartialAlias, 3 for MayAlias
-def execute_with_modifications(cmd: list[str], modify_index: int, change_to: int) -> bool:
+def execute_with_modifications(cmd: list[str], modify_index: int,
+                               change_to: int) -> bool:
 
     p = Popen(cmd, stdin=PIPE, stdout=PIPE, text=True)
     line = p.stdout.readline().strip()
@@ -79,7 +83,11 @@ def execute_with_modifications(cmd: list[str], modify_index: int, change_to: int
 
 
 def compile_file(index: int) -> bool:
-    cmd = ['../llvm-project/build_ast/bin/clang', '-Os', "files_sache/file" + str(index) + ".ll", '-o', "files_sache/file" + str(index) + ".out"]
+    cmd = [
+        '../llvm-project/build_ast/bin/clang', '-Os',
+        "files_sache/file" + str(index) + ".ll", '-o',
+        "files_sache/file" + str(index) + ".out"
+    ]
     p = run(cmd, stdout=DEVNULL, stderr=DEVNULL, text=True)
     return p.returncode == 0
 
@@ -94,40 +102,3 @@ def measure_outputsize(file: str) -> int:
     line_list = second_line.split("\t")
     # The other results are more info on where size is: test, data, bss
     return int(line_list[3])
-
-
-get_ll_file()
-
-cmd = ['../llvm-project//build_interact/bin/opt', '-Os', 'csmith/file2.ll', '-S']
-
-count = get_count_may_alias_queries(cmd)
-
-print("Count is " + str(count))
-
-for i in range(count):
-    if not execute_with_modifications(cmd, i, 1):
-        print("Modification failed for " + str(i))
-
-print("Done with modifications")
-
-# Compile groundtruth
-run(['../llvm-project/build_ast/bin/clang', '-Os', "csmith/file2.ll", '-o', "files_sache/file.out"], stdout=DEVNULL, stderr=DEVNULL, text=True)
-
-for i in range(count):
-    if i % 10 == 0:
-        print("Compiling " + str(i))
-    if not compile_file(i):
-        print("Compilation failed for " + str(i))
-
-print("Done with compilation")
-
-# Measure groundtruth
-true_size = measure_outputsize("files_sache/file.out")
-print("True size is " + str(true_size))
-
-for i in range(count):
-    size = measure_outputsize("files_sache/file" + str(i) + ".out")
-    if size == -1:
-        print("Size measurement failed for " + str(i))
-    elif size != true_size:
-        print(str(i) + ": " + str(size))
