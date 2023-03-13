ea16856fb856c0561a51a1ec611ab49d090e27bb
Michel Weber <michel.weber@inf.ethz.ch>
2023-03-13 13:56:02 +0100

    new test file, add modify

---
+++ csmith/test.c
@@ -0,0 +1,26 @@
+static int foo(int x, int *c);
+
+int main()
+{
+    int a = 128937;
+    int *b = &a;
+    return foo(0, b);
+}
+
+static int foo(int x, int *c)
+{
+    int b = 189724824;
+    int *d = &b;
+
+    if (x)
+    {
+        d = c;
+    }
+
+    if (c == d)
+    {
+        *d = 777177;
+    }
+
+    return *d;
+}
